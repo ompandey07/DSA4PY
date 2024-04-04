315fe50056eaeb41fc09f1d06165cdea154adcaa
ompandey07 <125279737+ompandey07@users.noreply.github.com>
2024-04-04 20:17:42 +0545

    yy
    yy

---
+++ main.py
@@ -0,0 +1,19 @@
+# print ('Python On Top')
+
+# Mutable and Immutable in Python
+
+
+# Mutable Datatypes : Yasto Datatypes jun Ki Change Garna sakinxa
+# examples
+# List
+# Set
+# Dictonary
+# Array
+
+# Immutable Datatypes : jun Ki change Garna Mildaina
+# Examples
+# String
+# Intiger
+# Floting Numbers 0.1
+#Boolien 
+#tupple
